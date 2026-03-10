eb0e4c750fd31cb31e748f387b9004497b9edc09
Jade <yjingshii@gmail.com>
2026-03-10 11:40:50 +1100

    3.2C

---
+++ packages/square_turtle_pkg/src/distance_turtle.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# Import Dependencies
+import rospy 
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+from turtlesim.msg import Pose
+import time 
+import math
+
+class DistanceReader:
+    def __init__(self):
+        
+        # Initialize the node
+        rospy.init_node('turtlesim_distance_node', anonymous=True)
+
+        # Initialize subscriber: input the topic name, message type and callback signature  
+        rospy.Subscriber("/turtle1/pose", Pose,self.callback)
+
+        # Initialize publisher: input the topic name, message type and msg queue size
+        self.distance_publisher = rospy.Publisher('/turtle_dist', Float64, queue_size=10)
+
+        # store info
+        self.prev_x = None
+        self.prev_y = None
+        self.total_distance = 0.0
+
+        # Printing to the terminal, ROS style
+        rospy.loginfo("Initalized node!")
+        
+        # This blocking function call keeps python from exiting until node is stopped
+        rospy.spin()
+
+    # Whenever a message is received from the specified subscriber, this function will be called
+    def callback(self,msg):
+        rospy.loginfo("Turtle Position: %s %s", msg.x, msg.y)
+
+        ########## YOUR CODE GOES HERE ##########
+        # Calculate the distance the turtle has travelled and publish it
+        # initial location
+        if self.prev is None:
+            self.prev_x = msg.prev_x
+            self.prev_y = msg.prev_y
+
+        # calc distance
+        dx = msg.prev_x - self.prev_x
+        dy = msg.prev_y - self.prev_y
+
+        distance = math.sqrt(dx**2 + dy**2)
+        total_distance += distance
+
+        # update location
+        self.prev_x = msg.prev_x
+        self.prev_y = msg.prev_y
+
+        # publish 
+        self.distance_publisher(self.total_distance)
+
+
+        ###########################################
+
+if __name__ == '__main__': 
+
+    try: 
+        distance_reader_class_instance = DistanceReader()
+    except rospy.ROSInterruptException: 
+        pass
+        
