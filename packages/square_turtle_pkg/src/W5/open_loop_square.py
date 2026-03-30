9d88d95c4b3de84b5af25d35ccb000ecc2525105
Jade <yjingshii@gmail.com>
2026-03-31 10:08:37 +1100

    4.1P

--- packages/square_turtle_pkg/src/W5/open_loop_square.py
+++ packages/square_turtle_pkg/src/W5/open_loop_square.py
@@ -13,8 +13,8 @@ class Drive_Square:
         rospy.init_node('drive_square_node', anonymous=True)
         
         #Initialize Pub/Subs
-        self.pub = rospy.Publisher('/akandb/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
-        rospy.Subscriber('/akandb/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
+        self.pub = rospy.Publisher('/mybota002409/car_cmd_switch_node/cmd', Twist2DStamped, queue_size=1)
+        rospy.Subscriber('/mybota002409/fsm_node/mode', FSMState, self.fsm_callback, queue_size=1)
         
     # robot only moves when lane following is selected on the duckiebot joystick app
     def fsm_callback(self, msg):
@@ -46,14 +46,30 @@ class Drive_Square:
         self.pub.publish(self.cmd_msg)
         rospy.loginfo("Forward!")
         rospy.sleep(1) # straight line driving time
+
+        self.cmd_msg.header.stamp = rospy.Time.now()
+        self.cmd_msg.v = 0.5 
+        self.cmd_msg.omega = 0.5
+        self.pub.publish(self.cmd_msg)
+        rospy.loginfo("Right!")
+        rospy.sleep(1) 
         
         self.cmd_msg.header.stamp = rospy.Time.now()
-        self.cmd_msg.v = -0.5 # striaght line velocity
+        self.cmd_msg.v = 0.5 # striaght line velocity
         self.cmd_msg.omega = 0.0
         self.pub.publish(self.cmd_msg)
-        rospy.loginfo("Backward!")
+        rospy.loginfo("Forward!")
         rospy.sleep(1) # straight line driving time
         
+
+        self.cmd_msg.header.stamp = rospy.Time.now()
+        self.cmd_msg.v = 0.5 
+        self.cmd_msg.omega = 0.5
+        self.pub.publish(self.cmd_msg)
+        rospy.loginfo("Right!")
+        rospy.sleep(1) 
+        
+
         ######################
                 
         self.stop_robot()
