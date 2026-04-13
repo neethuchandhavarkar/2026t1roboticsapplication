eb3837bab4a09cb133e4c0976124e5c7a3a087c0
Jade <yjingshii@gmail.com>
2026-04-13 16:28:29 +1000

    4.1P

--- packages/open_loop_square/src/credit.py
+++ packages/open_loop_square/src/credit.py
@@ -25,7 +25,7 @@ class ClosedLoopSquare:
 
         # Calibration change it according to the robot
         self.TICKS_PER_METER = 300
-        self.TICKS_PER_90_DEG = 100
+        self.TICKS_PER_90_DEG = 60
 
         self.cmd = Twist2DStamped()
 
