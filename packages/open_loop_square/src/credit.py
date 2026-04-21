242a2983f5b3d188cfd38ef400d218a895bf01f3
Jade <yjingshii@gmail.com>
2026-04-21 10:31:30 +1000

    4.3D

--- packages/open_loop_square/src/credit.py
+++ packages/open_loop_square/src/credit.py
@@ -34,7 +34,7 @@ class ClosedLoopController:
 
         # Calibration 
         self.TICKS_PER_METER = 330
-        self.TICKS_PER_90_DEG = 15
+        self.TICKS_PER_90_DEG = 20
 
         self.cmd = Twist2DStamped()
 
